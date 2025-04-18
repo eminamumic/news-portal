ec013681652c8dd7fe06fbf7b548be464b400fd6
eminamumic <emimumic1@gmail.com>
2025-04-18 21:42:59 +0200

    Add calendar, footer, navbar, news-table components

---
+++ src/app/components/news-table/news-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-table',
+  imports: [],
+  templateUrl: './news-table.component.html',
+  styleUrl: './news-table.component.css'
+})
+export class NewsTableComponent {
+
+}
