2a2c9cc3678fc030f561d96fda124b7fee8674d5
eminamumic <emimumic1@gmail.com>
2025-04-18 21:44:55 +0200

    Add app-routing.module.ts file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,6 @@
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent },
+];
